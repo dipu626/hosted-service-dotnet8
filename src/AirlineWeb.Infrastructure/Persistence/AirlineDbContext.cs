113aeddd61aa4a934b7e922ce2c4a599955f27b6
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-09-13 13:38:41 +0600

    Project setup, containers setup, sql database setup & sql database added.

---
+++ src/AirlineWeb.Infrastructure/Persistence/AirlineDbContext.cs
@@ -0,0 +1,33 @@
+﻿using AirlineWeb.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirlineWeb.Infrastructure.Persistence
+{
+    public class AirlineDbContext : DbContext
+    {
+        public AirlineDbContext(DbContextOptions<AirlineDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<WebhookSubscription> WebhookSubscriptions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            if (WebhookSubscriptions?.Any() == false)
+            {
+                WebhookSubscription subscription = new()
+                {
+                    Id = 1,
+                    WebhookURI = "dummyData",
+                    Secret = "dummyData",
+                    WebhookPublisher = "dummyData",
+                    WebhookType = "dummyData"
+                };
+
+                modelBuilder.Entity<WebhookSubscription>().HasData(subscription);
+            }
+        }
+    }
+}
